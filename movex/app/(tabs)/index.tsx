15e52024f7b16f129fefa34dd0de35de7703fd23
Auwalu Muhammad Izziddin <auwalmuhammad5559@gmail.com>
2025-11-16 13:14:44 -0800

    tab navigation

--- movex/app/(tabs)/index.tsx
+++ movex/app/(tabs)/index.tsx
@@ -1,10 +1,11 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
   <Text className="text-5xl text-accent font-bold">Welcome to Movex.</Text>
-</View>
+ </View>
 
   );
 }
