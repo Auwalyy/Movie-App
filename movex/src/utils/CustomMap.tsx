433f4f4fd40f1dad06669e091239abeaa6b30b16
Auwalu Muhammad Izziddin <auwalmuhammad5559@gmail.com>
2025-12-23 01:58:49 -0800

    COMMIT

---
+++ movex/src/utils/CustomMap.tsx
@@ -0,0 +1,61 @@
+export const customMapStyle = [
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.business",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  }
+]
+
+export const indiaIntialRegion = {
+  latitude: 9.0820,
+  longitude: 8.6753,
+  latitudeDelta: 12,
+  longitudeDelta: 12,
+}
